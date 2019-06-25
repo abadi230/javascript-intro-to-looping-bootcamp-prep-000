d27c6e71e69d744f128807f1a105f0e167298ef2
Abdullah Bajaman <ambajaman@gmail.com>
2019-06-25 14:24:16 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,36 +1,4 @@
-// function forLoop(array){
-// for (var i = 0; i<25; i++){
-//   array.push(`I am ${i} strange loop${i === 1 ? '' : 's'}.`);
-  
-// }
-//   return array
-// }
 
-// function whileLoop(n){
-//   while (n !== 0){
-//     for(n ; n > 0; n--){
-//       console.log(n)
-//     } 
-    
-//   }
-//   return 'done'
-// }
-
-// var i = 0;
-
-// function incrementVariable() {
-//   i = i + 1;
-//   return i;
-// }
-
-
-
-// function doWhileLoop(num){
-//   do {
-//   console.log("I run once regardless.");
-//   } while (incrementVariable() < num + 1);
-  
-// }
 /*Build a function forLoop. It takes an array as an argument. Start counting from 0, and, using a for loop, add a string to the array 25 times. But not just any string. If your i value is 1, add the string "I am 1 strange loop."; if your i value is anything else, add the string "I am ${i} strange loops.". (Remember flow control with if and else? And how do we interpolate i?) Then return the array.
 1) loops
        forLoop(array)
