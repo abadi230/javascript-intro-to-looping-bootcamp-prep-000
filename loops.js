f02b6add8aaf21fa65cc85d9b9468cb744c8f31e
Abdullah Bajaman <ambajaman@gmail.com>
2019-06-25 13:47:15 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -58,9 +58,13 @@ function whileLoop(number){
   return 'done'
 }
 
-/*
+/*Define a function called doWhileLoop in loops.js. The function should take an integer as an argument. Use the incrementVariable() function (you can copy it from this README) in the condition, and console log "I run once regardless." while incrementVariable() returns a number less than the parameter received. (Your condition might look something like incrementVariable() < num.) Remember that it should also console log when receiving 0 as a parameter because the do-while runs before the condition is checked.
 
 3) loops
        doWhileLoop(num)
          console logs "I run once regardless." 1 time when passed an integer of0 as a parameter.:
 */
+function doWhileLoop(number){
+  var i =0;
+  function
+}
